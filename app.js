dd222be21344b551913c40191dcbd4f01e67abef
jessyelisius <jecintaelisius2020@gmail.com>
2024-12-25 23:03:31 +0100

    api in one session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connectionDB = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 
 
 const app = express();
@@ -15,10 +16,18 @@ connectionDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+
+// Set view engine and views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //////////route//////////
-app.use('/users', require('./routes/user.routes'));
+app.use('/', require('./routes/user.routes'));
 app.use('/contacts', require('./routes/userContact.routes'));
 
 app.listen(port, () =>{
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,19 @@ const { createUser, LoginUser, Logout } = require('../controller/userContrl');
 const validateTokens = require('../middleware/jwtToken');
 const router = express.Router();
 
+router.get('/signup',(req, res) => {
+    res.render('signup');
+});
+
 router.post('/register',createUser);
+
+router.get('/login',(req, res) => {
+    res.render('login');
+});
+
 router.post('/login', LoginUser);
+
+
 // router.post('/logout:id',validateTokens, Logout);
 
 module.exports = router;
